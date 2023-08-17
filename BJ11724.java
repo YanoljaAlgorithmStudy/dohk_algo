4b78595bf5ced71ddd75c4b28373281b78468391
dohyung <dohk325@gmail.com>
2023-08-17 14:27:55 +0900

    BJ11724

---
+++ BJ11724.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ11724 {
+    final static int MAX = 1000 + 10;
+    static int N, M;
+    static boolean graph[][];
+    static boolean visited[];
+    static int answer = 0;
+
+    static void dfs(int vertex){
+        visited[vertex] = true;
+
+        for (int j = 1; j <= N; j++) {
+            if(visited[j] == false && graph[vertex][j] == true) {
+                dfs(j);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new boolean[MAX][MAX];
+        visited = new boolean[MAX];
+
+        int u, v;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            graph[u][v] = true;
+            graph[v][u] = true;
+        }
+
+        // dfs
+        for (int i = 1; i <= N; i++) {
+            if(visited[i] == false) {
+                dfs(i);
+                answer++;
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+
+        br.close();
+        bw.close();
+    }
+}
